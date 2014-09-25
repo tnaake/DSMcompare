2dd5080e8a463e0d5466907559b6da7265155d9a
*tnaake* <thomasnaake@gmx.de>
2014-09-25 11:24:49 +0200

    create workflow script

--- R/functions.R
+++ R/functions.R
@@ -1,3 +1,4 @@
+## to do
 ## a collection to store manual measurements
 ## a collection to store model values
 ## setClass("manualMeasure", )
@@ -7,22 +8,26 @@
     .len <- length(files)
     ans <- list()
     for (i in 1:.len)
-        ans[[i]] <- readShapePoints(files)
+        ans[[i]] <- readShapePoints(files[i])
     return(ans)
 }
 
 ## function to extract x, y, z and class from raw data
+## returns a list of data frames
 .cutData <- function(uncutfiles, 
-    columns = list("x_coord" = 1, "y_coord" = 2, "z_coord" = 3, "class" = 4)) {
+    columns = list("x_coord" = 1, "y_coord" = 2, "z_coord" = 3, "class" = 4), omit.class = FALSE) {
     .len <- length(uncutfiles)
     ans <- list()
     for (i in 1:.len) {
         .data <- uncutfiles[[i]]
-        .newAns <- matrix(nrow = dim(.data)[1], ncol = 4)
+       ## .newAns <- data.frame(row.names = 1:dim(.data)[1])
+        .newAns <- matrix(nrow = dim(.data)[1], ncol = ifelse(omit.class, 3, 4))
+        .newAns <- as.data.frame(.newAns)
         .newAns[, 1] <- as.data.frame(.data[, columns$x_coord])[, 1]
         .newAns[, 2] <- as.data.frame(.data[, columns$y_coord])[, 1]
-        .newAns[, 3] <- as.data.frame(.data[, columns$class])[, 1]
-        .newAns[, 4] <- as.data.frame(.data[, columns$z_coord])[, 1]
+        .newAns[, 3] <- as.data.frame(.data[, columns$z_coord])[, 1]
+        if (!omit.class)
+            .newAns[, 4] <- as.data.frame(.data[, columns$class])[, 1]
         ans[[i]] <- .newAns 
     }  
     return(ans)
@@ -36,11 +41,6 @@
 #' y-coordinates (second column), z-coordinates (third column) and
 #' (optionally) class (fourth column)
 #' @export
-
-mM <- list("/Users/thomasnaake/Documents/University/Bachelor/FVA/Daten/Messpunkte/Messpunkte_Petra1.shp",
-    "/Users/thomasnaake/Documents/University/Bachelor/FVA/Daten/Messpunkte/Messpunkte_Petra2.shp",
-    "/Users/thomasnaake/Documents/University/Bachelor/FVA/Daten/Messpunkte/Messpunkte_Petra3.shp")
-
 .meanManualMeasure <- function(manualMeasure) {
     if (!is.list(manualMeasure))
         stop("argument is not a list")
@@ -48,9 +48,6 @@ mM <- list("/Users/thomasnaake/Documents/University/Bachelor/FVA/Daten/Messpunkt
         stop ("argument is of length 0")
     .len <- length(manualMeasure)
     .meta <- manualMeasure[[1]]
-    .mean <- lapply(manualMeasure, "[", 3)
-    .mean <- as.data.frame(.mean)
-    .mean <- apply(.mean, MARGIN = 1, mean)
     
     .sub <- rep(NA, dim(.meta)[1])
     ans <- data.frame("x_coord" = .sub, "y_coord" = .sub, 
@@ -59,16 +56,21 @@ mM <- list("/Users/thomasnaake/Documents/University/Bachelor/FVA/Daten/Messpunkt
     ans[, 1] <- .meta[, 1]
     ## load column 2 with y-coordinates
     ans[, 2] <- .meta[, 2]
+    ## load column 4 with class
+    ans[, 4] <- .meta[, 4]
+    
+    .mean <- lapply(manualMeasure, "[", 3)
+    .mean <- as.data.frame(.mean)
+    .mean <- apply(.mean, MARGIN = 1, mean)
     ## load column 3 with mean heights 
     ans[, 3] <- .mean
-    ## load column 4 with class
-    ans[, 4] <- .meta[, 3]
-    
     return(ans)
 }
 
 
-
+## function to calculate model calues at given coordinates
+## model takes x/y/z coords of a model
+## coordinates: data frame of coordinates (1st column x, 2nd column y values)
 .calculateModelValues <- function(coordinates, model, method = c("2D", "IDW"),
                                     idw = list("p" = 2, "m" = 5, "rad" = 5)) {
     .lenCoord <- dim(coordinates)[1]
@@ -94,9 +96,9 @@ mM <- list("/Users/thomasnaake/Documents/University/Bachelor/FVA/Daten/Messpunkt
         ## exponent according to Shepard, 1968
         .p <- idw$p 
         ## number of points which are used
-        .m <- iwd$m
+        .m <- idw$m
         ## use points which are iwd$rad m far from the coordinate point
-        .rad <- iwd$rad
+        .rad <- idw$rad
         
         ## vector which will contain height
         .zIDW <- vector("numeric", length = .lenCoord)
@@ -109,44 +111,43 @@ mM <- list("/Users/thomasnaake/Documents/University/Bachelor/FVA/Daten/Messpunkt
             ## ordering of the Di by increasing distance from P
             .Cpn <- sort(.Cp) 
             if (length(.Cp) < .m)
-                .Cpn <- .Cpn[1:length(.Cpn)]
+                .Cpn <- .Cpn[1:length(.Cp)]
             else
-                .Cpn <- .Cpn[1:m]
+                .Cpn <- .Cpn[1:.m]
             ## vector for weighting factors
             .method <- vector("numeric", length(.Cpn))
             if (length(.Cpn) != 0 && !is.na(.Cpn[1])) {
                 for (j in 1:length(.Cpn)) {
                     if (.Cpn[j] > 0 && .Cpn[j] <= .rad/3)
-                        .method <- "m1"
+                        .method[j] <- "m1"
                     if (.Cpn[j] > .rad/3 && .Cpn[j] <= .rad)
-                        .method <- "m2"
+                        .method[j] <- "m2"
                     if (.Cpn[j] > .rad)
                         .method[j] <- "m3"
                 }
                 ## matrix whose diagonal axis bears indices of points .Cpn i 
                 .matInd <- matrix(NA, length(.Cpn), length(.Cpn))
                 for (j in 1:length(.Cpn)) {
-                    if (q == 1) {
+                    if (j == 1) {
                         .vec <- which(.Cpn[j] == .dist)[1]
                         .vec <- c(rep(NA, j - 1), 
                                   .vec, 
                                   rep(NA, 10 - length(.vec)))
                         .matInd[j, j] <- .vec[j]
-                        .n <- 2
+                        n <- 2 ## was 2 ??
                     }
                     if (j > 1) {
                         .vec <- which(.Cpn[j] == .dist)
-                        if (vec[n - 1] == .matInd[j - 1, j - 1]) {
-                            .matInd[j, j] <- vec[j]
+                        if (.vec[n - 1] == .matInd[j - 1, j - 1]) {
+                            .matInd[j, j] <- .vec[n]
                             n <- n + 1
                             if (n == length(.vec) + 1)
                                 n <- 2 
-                        } else {
-                            .matInd[j, j] <- vec[1]
-                            n <- 2
-                        }
+                        } else 
+                            .matInd[j, j] <- .vec[1]; n <- 2
+                    
                     }
-                }
+                } ## end for loop
                 ## vector which contains indices of .matInd
                 .ind <- vector("numeric", dim(.matInd)[1])
                 for (j in 1:length(.ind))
@@ -156,16 +157,17 @@ mM <- list("/Users/thomasnaake/Documents/University/Bachelor/FVA/Daten/Messpunkt
                 .n <- 1
                 for (j in .ind) {
                     .n <- .n 
-                    if (.method[n] == "m1")
-                        .wf[.n] <- 1/sqrt((.coordinates[i, 1] - model[j, 1])^2 +
-                            (.coordinates[i, 2] - model[j, 2])^2)^.p
+                    if (.method[.n] == "m1")
+                        .wf[.n] <- 1/ sqrt( 
+                            (coordinates[i, 1] - model[j, 1])^2 + (coordinates[i, 2] - model[j, 2])^2
+                        ) ^ .p
                     if (.method[.n] == "m2")
                         .wf[.n] <- (27/(4*.rad) * (sqrt(
-                            (.coordinates[i, 1] - model[j, 1])^2 + 
-                                (.coordinates[i, 2] - model[j, 2])^2) / rad - 1))^.p
+                            (coordinates[i, 1] - model[j, 1])^2 + 
+                                (coordinates[i, 2] - model[j, 2])^2) / .rad - 1))^.p
                     if (.method[.n] == "m3")
                         .wf[.n] <- 0
-                    n <- n + 1
+                    .n <- .n + 1
                 }
                 ## if distance of next point == 0 to reference point (identical)
                 ## take this height
@@ -187,7 +189,8 @@ mM <- list("/Users/thomasnaake/Documents/University/Bachelor/FVA/Daten/Messpunkt
             }
                 
         }
-        ## END: IDW ## 
+    } ## END: IDW ## 
+    return(.mat)
 }
 
 
@@ -197,15 +200,17 @@ mM <- list("/Users/thomasnaake/Documents/University/Bachelor/FVA/Daten/Messpunkt
                               idw = list("p" = 2, "m" = 5, "rad" = 5)) {
     
     .lenCoord <- dim(coordinates)[1]
-    .mat <- (NA, nrow = .lenCoord, ncol = 2*length(model))
+    .mat <- matrix(NA, nrow = .lenCoord, ncol = 2*length(model))
     for (i in 1:length(model)) {
-        .values <- .meanManualMeasure(coordinates = coordinates, model = model, 
+        .values <- .calculateModelValues(coordinates = coordinates, model = model[[i]], 
                         method = method, idw = list(idw$p, idw$m, idw$rad))
         .mat[, i] <- .values[, 1]
         .mat[, i + 1] <- .values[, 2]
     }
     return(.mat)
 }
+
+
 
 .errorModel <- function(manual, model) {
     ans <- manual - model
